98aac1e2e3e0916a1650275a5582240e5f78808b
Miacarmen <maitreyacarmen@gmail.com>
2022-06-01 16:24:15 -0700

    deleted external stylesheets for nav and footer

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,41 +1,44 @@
-import React from 'react';
-import { Card } from 'react-daisyui'
-import p1 from '../images/GirlyNoteTaker.png';
-import p2 from '../images/DayPlanner.png';
-import p3 from '../images/MiseEnPlace.png';
+import React from "react";
+import { Card } from "react-daisyui";
+import p1 from "../images/GirlyNoteTaker.png";
+import p2 from "../images/DayPlanner.png";
+import p3 from "../images/MiseEnPlace.png";
 
-// * TO-DO: add SIX  projects 
+// * TO-DO: add SIX  projects
 // * TO-DO: add links to repo and deployed site
 
 const Portfolio = () => {
-    return (
-        <div className="container max-w-screen-lg mx-auto pb-10 text-primary">
-            <Card>
-        <h1 className="mt-7 font-bold text-2xl">A Cute and Girly Notetaker</h1>
-        <p>Take Notes with Express, but in a Cute Girly Way</p>
-        {/* <a href="https://github.com/Miacarmen/super_girly_note_taker" target="blank" className="text-secondary">Link to
+  return (
+    <div className="bg-base-200">
+      <div className="container max-w-screen-lg mx-auto pb-10 text-primary">
+        <Card>
+          <h1 className="mt-7 font-bold text-2xl">
+            A Cute and Girly Notetaker
+          </h1>
+          <p>Take Notes with Express, but in a Cute Girly Way</p>
+          {/* <a href="https://github.com/Miacarmen/super_girly_note_taker" target="blank" className="text-secondary">Link to
             Github</a>
         <a href="https://super-girly-notetaker.herokuapp.com/" target="blank" className="text-secondary">Live Site Here</a> */}
-        <img alt="Girly Note Taker" src={p1}></img>
+          <img alt="Girly Note Taker" src={p1}></img>
         </Card>
 
         <Card>
-        <h1 className="mt-7 font-bold text-2xl">Day Planner</h1>
-        <p>Plan Your Day Using moment.js</p>
-        {/* <a href="https://github.com/Miacarmen/Day-Planner" target="blank" className="text-secondary">Link to Github</a> */}
-        <img alt="Day Planner" src={p2}></img>
+          <h1 className="mt-7 font-bold text-2xl">Day Planner</h1>
+          <p>Plan Your Day Using moment.js</p>
+          {/* <a href="https://github.com/Miacarmen/Day-Planner" target="blank" className="text-secondary">Link to Github</a> */}
+          <img alt="Day Planner" src={p2}></img>
         </Card>
         <Card>
-        <h1 className="mt-7 font-bold text-2xl">Mise en Place</h1>
-        <p>Time Management Software</p>
-        {/* <a href="https://github.com/Miacarmen/mise-en-place" target="blank" className="text-secondary">Link to
+          <h1 className="mt-7 font-bold text-2xl">Mise en Place</h1>
+          <p>Time Management Software</p>
+          {/* <a href="https://github.com/Miacarmen/mise-en-place" target="blank" className="text-secondary">Link to
             Github</a><br>
         <a href="https://mise-en-place-planner.herokuapp.com/" target="blank" className="text-secondary">Live Site Here</a> */}
-        <img alt="Mise en Place" src={p3}></img>
+          <img alt="Mise en Place" src={p3}></img>
         </Card>
-        </div>
-    )
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
-
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,2 +1,4 @@
+import React from "react";
+import { Card } from "react-daisyui";
 // * TO-DO: When this page is active, display shopping cart icon
 // * TO-DO: When a user clicks add to cart, image is added to cart page
