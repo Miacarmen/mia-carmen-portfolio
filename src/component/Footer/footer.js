3ff6f99e5d561f58c1b78f3dde288bbada707cfb
Miacarmen <maitreyacarmen@gmail.com>
2022-06-28 00:58:53 -0700

    footer reformatted

--- src/component/Footer/footer.js
+++ src/component/Footer/footer.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { Footer } from 'react-daisyui';
 import image2 from '../../images/heart (9).png';
 import icon1 from '../../images/github.png';
 import icon2 from '../../images/linkedin.png';
 import icon3 from '../../images/instagram.png';
-// * TO-DO: add Social icons with links to profiles
 
 const Foot = ({ currentPage, handlePageChange }) => {
   return (
-    <Footer className='footer footer-center p-10 bg-base-100  rounded '>
-      <div>
-        <div className='font-semibold text-lg'>
-          <Footer.Title>Company</Footer.Title>
+    <div>
+      <footer class='footer p-10 bg-base-100 text-base-content justify-items-center'>
+        <div>
+          <span class='footer-title'>Services</span>
+          <p>Web Development</p>
+          <p>Graphic Design</p>
+          <p>Illustration</p>
+          <p>Branding</p>
         </div>
-        <div className='md:flex items-stretch space-x-4 mb-4 mt-4'>
+        <div>
+          <span class='footer-title'>Company</span>
           <a
             href='#home'
             onClick={() => handlePageChange('Home')}
@@ -63,33 +66,36 @@ const Foot = ({ currentPage, handlePageChange }) => {
             Contact
           </a>
         </div>
-        <div className='mt-3 font-semibold text-lg'>
-          <Footer.Title>Services</Footer.Title>
+
+        <div>
+          <span class='footer-title'>Legal</span>
+          <a href='/' class='link link-hover'>
+            Terms of use
+          </a>
+          <a href='/' class='link link-hover'>
+            Privacy policy
+          </a>
+          <a href='/' class='link link-hover'>
+            Cookie policy
+          </a>
         </div>
-        <div className='my-4'>
-          <p>Web Development</p>
-          <p>Graphic Design</p>
-          <p>Illustration</p>
-          <p>Branding</p>
+      </footer>
+      <footer class='footer flex grid grid-cols-3 px-10 pt-2 pb-2 bg-base-100 text-base-content border-base-300'>
+        <div class='place-self-center grid-flow-col'>
+          <p>Copyright © 2022 - Made With Sugar, Spice & Everything Nice</p>
         </div>
-        <div className='my-3 font-semibold text-lg'>
-          <Footer.Title>Social</Footer.Title>
-        </div>
-        <div className='md:flex items-stretch space-x-4 mb-4 mt-4'>
-          <img alt='github' src={icon1} height='40px' width='50px'></img>
-          <img alt='linkedin' src={icon2} height='40px' width='50px'></img>
-          <img alt='instagram' src={icon3} height='40px' width='50px'></img>
-        </div>
-      </div>
-      <div>
-        <div className='grid grid-flow-col gap-4'>
+        <div className='justify-self-center'>
           <img alt='logo' src={image2} height='70px' width='70px'></img>
         </div>
-      </div>
-      <div>
-        <p>Copyright © 2022 - Made With Sugar, Spice & Everything Nice</p>
-      </div>
-    </Footer>
+        <div class='md:place-self-center md:justify-self-end'>
+          <div class='grid grid-flow-col gap-4'>
+            <img alt='github' src={icon1} height='30px' width='30px'></img>
+            <img alt='linkedin' src={icon2} height='30px' width='30px'></img>
+            <img alt='instagram' src={icon3} height='30px' width='30px'></img>
+          </div>
+        </div>
+      </footer>
+    </div>
   );
 };
 
